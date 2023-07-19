d6ee6e5b3c9b2d1deeb49aadb3c565118d51d719
Udayl56 <105300620+Udayl56@users.noreply.github.com>
2023-07-19 14:54:05 +0530

    Update DPWApp.py

--- DPWApp.py
+++ DPWApp.py
@@ -38,9 +38,9 @@ def main():
     
     Pregnancies=st.slider('Number of Pregnancies',min_value=0,max_value=20,value=4,step=1)
     Glucose=st.slider('Glucose level',min_value=0,max_value=199,value=60,step=1)
-    BloodPressure=st.slider('BloodPressure level',min_value=0,max_value=140,value=80,step=1)
-    SkinThickness=st.slider('SkinThickness value',min_value=0,max_value=100,value=40,step=1)
-    Insulin=st.slider('Insulin level',min_value=0,max_value=1000,value=400,step=1)
+    BloodPressure=st.slider('BloodPressure level in mm Hg',min_value=0,max_value=140,value=80,step=1)
+    SkinThickness=st.slider('SkinThickness in mm',min_value=0,max_value=100,value=40,step=1)
+    Insulin=st.slider('Insulin level in mu U/ml',min_value=0,max_value=1000,value=400,step=1)
     BMI=st.slider('BMI value',min_value=0.0,max_value=70.0,value=33.3,step=0.01)
     DiabetesPedigreeFunction=st.slider('Diabetes Pedigree Function value',min_value=0.000, step=0.001, max_value=3.0, value=0.045, format="%3f")
     Age=st.slider('Age of a person',min_value=10,max_value=100,value=21,step=1)
